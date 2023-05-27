9e3e2e3fc210986af303e93ae81c50f1dfece972
DiazAdriel0 <diazadriel0@gmail.com>
2023-05-27 14:04:57 -0300

    Completed homework

---
+++ browser/app.js
@@ -0,0 +1,27 @@
+//(function () {
+//let whiteboard = window.whiteboard;
+const whiteboard = require("./whiteboard")
+let socket = window.io(window.location.origin);
+
+socket.on("connect", function () {
+  console.log("Connected!");
+});
+
+socket.on("load", function (strokes) {
+  strokes.forEach(function (stroke) {
+    let start = stroke.start;
+    let end = stroke.end;
+    let color = stroke.color;
+    whiteboard.draw(start, end, color, false);
+  });
+});
+
+socket.on("draw", function (start, end, color) {
+  whiteboard.draw(start, end, color, false);
+});
+
+whiteboard.on("draw", function (start, end, color) {
+  socket.emit("draw", start, end, color);
+});
+//})();
+
